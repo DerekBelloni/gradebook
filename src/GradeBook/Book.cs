e5e5ee6d9bbb846126126fbdb6879ff83a458774
Derek <derekbelloni10@gmail.com>
2022-05-18 14:54:39 -0600

    first type test written

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -8,7 +8,7 @@ namespace GradeBook
     public Book(string name)
     {
       grades = new List<double>();
-      this.name = name;
+      Name = name;
     }
     public void AddGrade(double grade)
     {
@@ -29,14 +29,14 @@ namespace GradeBook
         result.High = Math.Max(grade, result.High);
         result.Average += grade;
       }
-        result.Average /= grades.Count;
-        return result;
+      result.Average /= grades.Count;
+      return result;
     }
 
 
 
     private List<double> grades;
-    private string name;
+    public string Name;
 
 
   }

2100663f8640d5b51386f908aa0b4440e2bca6fe
Derek <derekbelloni10@gmail.com>
2022-05-18 10:46:25 -0600

    commit

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -8,6 +8,12 @@ public class UnitTest1
   [Fact]
   public void Test1()
   {
-    Assert.Equal()
+    var x = 5;
+    var y = 2;
+    var expected = 10;
+    var actual = x * y;
+
+
+    Assert.Equal(expected, actual);
   }
 }

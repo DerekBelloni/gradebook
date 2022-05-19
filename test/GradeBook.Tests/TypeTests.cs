e55b6a14d8d5f13ac0e8acdbc4cdbe360dc62ff7
Derek <derekbelloni10@gmail.com>
2022-05-19 14:51:18 -0600

    pass by ref unit test written

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,54 @@ namespace GradeBook.Tests;
 public class TypeTests
 {
   [Fact]
+  public void CanSetNameFromReference()
+  {
+    var book1 = GetBook("Book 1");
+    SetName(book1, "New Name");
+
+    Assert.Equal("New Name", book1.Name);
+
+  }
+
+  private void SetName(Book book, string name)
+  {
+    book.Name = name;
+  }
+
+  [Fact]
+  public void CSharpIsPassByValue()
+  {
+    var book1 = GetBook("Book 1");
+    GetBookSetName(book1, "New Name");
+
+    Assert.Equal("Book 1", book1.Name);
+
+  }
+
+  private void GetBookSetName(Book book, string name)
+  {
+    book = new Book(name);
+    book.Name = name;
+  }
+
+  [Fact]
+  public void CSharpCanPassByReference()
+  {
+    var book1 = GetBook("Book 1");
+    GetBookSetName(ref book1, "New Name");
+
+    Assert.Equal("New Name", book1.Name);
+
+  }
+
+  private void GetBookSetName(ref Book book, string name)
+  {
+    book = new Book(name);
+
+  }
+
+
+  [Fact]
   public void GetBookReturnDifferentObjects()
   {
     var book1 = GetBook("Book 1");
@@ -13,14 +61,14 @@ public class TypeTests
 
     Assert.Equal("Book 1", book1.Name);
     Assert.Equal("Book 2", book2.Name);
-
+    Assert.NotSame(book1, book2);
   }
 
   [Fact]
-  public void GetSameBookDifferentVairables()
+  public void GetSameBookDifferentVariables()
   {
     var book1 = GetBook("Book 2");
-    var book2 = GetBook("Book 2");
+    var book2 = book1;
 
     Assert.Equal("Book 2", book1.Name);
     Assert.Equal("Book 2", book2.Name);
